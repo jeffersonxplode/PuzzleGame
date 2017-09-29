eec60d7148766d16495c23c8e96973d1c25f7b98
Jefferson Augusto <jeffersonxplode@gmail.com>
2017-09-28 21:32:38 -0300

    Inital Commit

---
+++ PuzzleGame/Source/PuzzleGame/PuzzleGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEGAME_API APuzzleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
